2342e68496c858a5fbf91799e4f2d2890c51ff09
“pablonap” <pnapoli33@gmail.com>
2023-09-23 15:34:51 -0300

    Initial commit

---
+++ src/main/java/com/trafilea/coffeeshop/CoffeeShopEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.trafilea.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trafilea/coffeeshop/CoffeeShopEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafilea.coffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
