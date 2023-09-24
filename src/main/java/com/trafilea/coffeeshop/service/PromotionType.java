2416d06874eb72cec9bcb44deb1c6504735fad35
“pablonap” <pnapoli33@gmail.com>
2023-09-24 18:26:59 -0300

    Add itShouldCreateOrderWithExtraCoffeePromotion test case; fix logic for adding an extra coffee product.

--- src/main/java/com/trafilea/coffeeshop/service/PromotionType.java
+++ src/main/java/com/trafilea/coffeeshop/service/PromotionType.java
@@ -1,25 +1,28 @@
 package com.trafilea.coffeeshop.service;
 
-import com.trafilea.coffeeshop.exception.ExceptionMessages;
-import com.trafilea.coffeeshop.exception.PromotionApplicationException;
 import com.trafilea.coffeeshop.model.CartProduct;
 import com.trafilea.coffeeshop.model.Order;
+import com.trafilea.coffeeshop.model.Product;
 import com.trafilea.coffeeshop.model.ProductCategory;
 
-import java.util.Optional;
+import java.util.HashSet;
+import java.util.Set;
 
 public enum PromotionType {
     EXTRA_FOR_FREE {
         @Override
         public void applyPromotion(Order order) {
-            Optional<CartProduct> coffeeProduct = order.getCart().getCartProducts()
-                    .stream()
-                    .filter(cp -> ProductCategory.COFFEE.getDescription().equals(cp.getProduct().getCategory()))
-                    .findFirst();
+            Product product = new Product();
+            product.setId(1L);
+            product.setName("extra coffee for free");
+            product.setPrice(0.0);
+            product.setCategory(ProductCategory.COFFEE.getDescription());
+            CartProduct cartProduct = CartProduct.cartProductOf(order.getCart(), product, 1) ;
 
-            CartProduct cartProduct = coffeeProduct
-                    .orElseThrow(() -> new PromotionApplicationException(ExceptionMessages.PROMOTION_APPLICATION_EXCEPTION.getMessage()));
-            cartProduct.addToQuantity(1);
+            Set<CartProduct> cartProducts = new HashSet<>();
+            order.getCart().getCartProducts().forEach(e-> cartProducts.add(e));
+            cartProducts.add(cartProduct);
+            order.getCart().setCartProducts(cartProducts);
         }
     },
     FREE_SHIPPING {
--- src/test/java/com/trafilea/coffeeshop/service/PlaceOrderServiceTests.java
+++ src/test/java/com/trafilea/coffeeshop/service/PlaceOrderServiceTests.java
@@ -43,6 +43,10 @@ public class PlaceOrderServiceTests {
     @Test
     void itShouldCreateOrderWithoutAnyPromotion() {
         // given
+        final int QUANTITY_PRODUCT_1 = 1;
+        final int QUANTITY_PRODUCT_2 = 3;
+        final int QUANTITY_PRODUCT_3 = 1;
+
         OrderRequestDto orderRequestDto = TestsUtils.orderRequestDtoOf(1L, "velazquez 1234");
 
         Product product1 = TestsUtils.productOf(
@@ -65,9 +69,9 @@ public class PlaceOrderServiceTests {
 
         Cart cart = TestsUtils.cartOf(1L, 2);
 
-        CartProduct cartProduct1 = TestsUtils.cartProductOf(1L, cart, product1, 1);
-        CartProduct cartProduct2 = TestsUtils.cartProductOf(2L, cart, product2, 3);
-        CartProduct cartProduct3 = TestsUtils.cartProductOf(3L, cart, product3, 1);
+        CartProduct cartProduct1 = TestsUtils.cartProductOf(1L, cart, product1, QUANTITY_PRODUCT_1);
+        CartProduct cartProduct2 = TestsUtils.cartProductOf(2L, cart, product2, QUANTITY_PRODUCT_2);
+        CartProduct cartProduct3 = TestsUtils.cartProductOf(3L, cart, product3, QUANTITY_PRODUCT_3);
 
         Set<CartProduct> cartProducts = Set.of(cartProduct1, cartProduct2, cartProduct3);
 
@@ -77,14 +81,14 @@ public class PlaceOrderServiceTests {
         product3.setCartProducts(cartProducts);
 
         Order order = new Order(cart);
-        order.setShippingAddress("Shipping Address");
+        order.setShippingAddress(orderRequestDto.shippingAddress());
 
         when(cartRepository.findById(orderRequestDto.cartId())).thenReturn(Optional.of(cart));
         when(orderRepository.save(Mockito.any(Order.class))).thenReturn(order);
         when(orderResponseDtoMapper.apply(order)).thenReturn(Mockito.any(OrderResponseDto.class));
 
         // when
-        OrderResponseDto result = underTest.createOrder(orderRequestDto);
+        underTest.createOrder(orderRequestDto);
 
         // then
         ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
@@ -93,6 +97,52 @@ public class PlaceOrderServiceTests {
 
         assertThat(capturedOrder.getProductsAmount()).isEqualTo(5);
         assertThat(capturedOrder.getTotalAmount()).isEqualTo(74.0); // 69 + 5 (from shipping)
+        assertThat(capturedOrder.isFreeShipping()).isFalse();
+        assertThat(capturedOrder.getDiscount()).isEqualTo(0.0);
+
+        verify(cartRepository, times(1)).findById(orderRequestDto.cartId());
+        verify(orderRepository, times(1)).save(Mockito.any(Order.class));
+        verify(orderResponseDtoMapper, times(1)).apply(order);
+    }
+
+    @Test
+    void itShouldCreateOrderWithExtraCoffeePromotion() {
+        // given
+        final int QUANTITY = 2;
+        OrderRequestDto orderRequestDto = TestsUtils.orderRequestDtoOf(1L, "velazquez 1234");
+
+        Product product1 = TestsUtils.productOf(
+                1L,
+                "american coffee",
+                4.0,
+                ProductCategory.COFFEE.getDescription());
+
+        Cart cart = TestsUtils.cartOf(1L, 2);
+
+        CartProduct cartProduct1 = TestsUtils.cartProductOf(1L, cart, product1, QUANTITY);
+
+        Set<CartProduct> cartProducts = Set.of(cartProduct1);
+
+        cart.setCartProducts(cartProducts);
+        product1.setCartProducts(cartProducts);
+
+        Order order = new Order(cart);
+        order.setShippingAddress(orderRequestDto.shippingAddress());
+
+        when(cartRepository.findById(orderRequestDto.cartId())).thenReturn(Optional.of(cart));
+        when(orderRepository.save(Mockito.any(Order.class))).thenReturn(order);
+        when(orderResponseDtoMapper.apply(order)).thenReturn(Mockito.any(OrderResponseDto.class));
+
+        // when
+        underTest.createOrder(orderRequestDto);
+
+        // then
+        ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
+        verify(orderRepository).save(orderArgumentCaptor.capture());
+        Order capturedOrder = orderArgumentCaptor.getValue();
+
+        assertThat(capturedOrder.getProductsAmount()).isEqualTo(3);
+        assertThat(capturedOrder.getTotalAmount()).isEqualTo(13); // 4 (from coffee) + 5 (from shipping)
         assertThat(capturedOrder.isFreeShipping()).isFalse();
         assertThat(capturedOrder.getDiscount()).isEqualTo(0.0);
 
